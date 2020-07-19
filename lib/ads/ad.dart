a2dc9ddd211ba4a4e381846dc7c60f700ba4dd77
Islam <skyart450@gmail.com>
2020-07-19 12:21:04 +0500

    keys publish icons

--- lib/ads/ad.dart
+++ lib/ads/ad.dart
@@ -4,67 +4,69 @@ import 'package:google_translate/ads/ad_manager.dart';
 class Ad {
   static const MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
     nonPersonalizedAds: true,
-    keywords: <String>['any'],
+    keywords: <String>['any', 'movies', 'ads'],
   );
 
- static BannerAd bannerAd;
- static InterstitialAd interstitialAd;
+  static BannerAd bannerAd;
+  static InterstitialAd interstitialAd;
 
- static BannerAd createBannerAd() {
+  static BannerAd createBannerAd() {
     return BannerAd(
-//        adUnitId: AdManager.bannerAdUnitId,
-        adUnitId: BannerAd.testAdUnitId,
-        size: AdSize.banner,
-        targetingInfo: targetingInfo,
-        listener: (MobileAdEvent event) {
+      adUnitId: AdManager.bannerAdUnitId,
+//      adUnitId: BannerAd.testAdUnitId,
+      size: AdSize.banner,
+      targetingInfo: targetingInfo,
+//        listener: (MobileAdEvent event) {
 //          if (event == MobileAdEvent.failedToLoad) {
 //            bannerAd..load();
 //          } else if (event == MobileAdEvent.closed) {
 //            bannerAd = createBannerAd()..load();
 //          }
-        });
+//        }
+    );
   }
+
   static void hideBannerAd() async {
     await bannerAd.dispose();
     bannerAd = null;
   }
- static InterstitialAd createInterstitialAd() {
+
+  static InterstitialAd createInterstitialAd() {
     return InterstitialAd(
-//        adUnitId: AdManager.interstitialAdUnitId,
-        adUnitId: InterstitialAd.testAdUnitId,
-        targetingInfo: targetingInfo,
-        listener: (MobileAdEvent event) {
+      adUnitId: AdManager.interstitialAdUnitId,
+//        adUnitId: InterstitialAd.testAdUnitId,
+      targetingInfo: targetingInfo,
+//        listener: (MobileAdEvent event) {
 //          if (event == MobileAdEvent.failedToLoad) {
 //            interstitialAd..load();
 //          } else if (event == MobileAdEvent.closed) {
 //            interstitialAd = createInterstitialAd()..load();
 //          }
-        });
+//        }
+    );
   }
 
- static void initState() {
-//    FirebaseAdMob.instance.initialize(appId: AdManager.appId);
-    FirebaseAdMob.instance.initialize(appId: FirebaseAdMob.testAppId);
-
+  static void initState() {
+    FirebaseAdMob.instance.initialize(appId: AdManager.appId);
+//    FirebaseAdMob.instance.initialize(appId: FirebaseAdMob.testAppId);
   }
 
- static void initInterstitialAd() {
+  static void initInterstitialAd() {
     interstitialAd = createInterstitialAd()
       ..load()
       ..show();
   }
 
- static void initBanner() {
+  static void initBanner() {
     bannerAd = createBannerAd()
       ..load()
       ..show();
   }
 
-  static dispose() async{
- await   bannerAd?.dispose();
- bannerAd=null;
-   await interstitialAd?.dispose();
- interstitialAd=null;
-
-   }
+  static dispose() async {
+    await bannerAd?.dispose();
+    bannerAd = null;
+    await interstitialAd?.dispose();
+    interstitialAd = null;
+  }
 }
--- lib/ads/ad_manager.dart
+++ lib/ads/ad_manager.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 class AdManager {
-
   static String get appId {
     if (Platform.isAndroid) {
       return "ca-app-pub-3823537053576178~4633880080";
